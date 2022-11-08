3074b90ff144f182ecd58e0976cb4c552473152d
Neda <nedachangizi@gmail.com>
2022-11-08 10:25:08 -0800

    do/while

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,36 @@
 // alert("this is fun");
 // console.log("this is really fun")
-// for (let i = 0 ; i <10000; i++)
+// for (let i = 0 ; i <1000000; i++)
 // {
-//     console.log(i)
+//     console.log(i);
+
 // }
 
-for (let i = 1; i <=10 ; i++)
-{
-    console.log(Math.pow(2,i))
-    // console.log(2 ** i)
-}
+// for (let i = 1; i <=10 ; i++)
+// {
+//     console.log(Math.pow(2,i));
+//     // console.log(2 ** i);
+// }
 
-let i =1;
-while( i <=10)
-{
-    console.log(Math.pow(2,i))
-    // console.log(2 ** i)
-    i++;
-}
+// let i =11;
+// while( i <=10)
+// {
+//     console.log(Math.pow(2,i))
+//     // console.log(2 ** i)
+//     i++;
+// }i =11;
+// while( i <=10)
+// {
+//     console.log(Math.pow(2,i))
+//     // console.log(2 ** i)
+//     i++;
+// }
+
+let input = "";
+
+do {
+  input = prompt("What is your name?");
+  // prompt always returns string.
+  //we need to convert the string to a number and check if it was successfully converted
+} while (!isNaN(Number(input)));
+console.log(input);
