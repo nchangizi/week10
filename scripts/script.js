aad929ad3f85719f201360e01aadcf9fee849919
Neda <nedachangizi@gmail.com>
2022-11-08 09:54:00 -0800

    loop practice

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// alert("this is fun");
+// console.log("this is really fun")
+// for (let i = 0 ; i <10000; i++)
+// {
+//     console.log(i)
+// }
+
+for (let i = 1; i <=10 ; i++)
+{
+    console.log(Math.pow(2,i))
+    // console.log(2 ** i)
+}
+
+let i =1;
+while( i <=10)
+{
+    console.log(Math.pow(2,i))
+    // console.log(2 ** i)
+    i++;
+}
