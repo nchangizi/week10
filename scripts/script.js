b3130c14a21fa078f599a3620ef8226cb7ed8f2b
Neda <nedachangizi@gmail.com>
2022-11-08 10:54:43 -0800

    array and obj example

--- scripts/script.js
+++ scripts/script.js
@@ -26,14 +26,43 @@
 //     i++;
 // }
 
-let input = "";
+// let input = "";
 
-do {
-  input = prompt("What is your name?");
-  // prompt always returns string.
-  //we need to convert the string to a number and check if it was successfully converted
-} while (!isNaN(Number(input)));
-// console.log(input);
+// do {
+//   input = prompt("What is your name?");
+//   // prompt always returns string.
+//   //we need to convert the string to a number and check if it was successfully converted
+// } while (!isNaN(Number(input)));
+// // console.log(input);
 
-// alert("Welcome " + input);
-alert(`Welcome ${input}`)
+// // alert("Welcome " + input);
+// alert(`Welcome ${input}`);
+
+let fruits = ["Apple", "Banana"];
+let newLength = fruits.push("Orange");
+
+let poppedElem = fruits.pop();
+
+fruits.unshift("Mango");
+
+fruits.shift();
+
+// for (let i = 0; i <fruits.length ; i++)
+// {
+//     console.log(fruits[i]);
+// }
+
+// for (let item of fruits)
+// {
+//     console.log(item);
+// }
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  gender: "male",
+  interests: ["music", "skiing"],
+};
+
+console.log(
+  `My name is ${person.name[0]} ${person.name[1]}. I am interested in ${person.interests[0]} and ${person.interests[1]}`
+);
